55b99b724ce4f2c634ae59cbd598cf96654cc8ae
Nikita Melnikov <nikita.melnikov@and.digital>
2019-10-03 14:07:21 +0100

    Fixed some of the styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 //import { thisTypeAnnotation } from '@babel/types';
 import MainBody from './components/MainBody'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 class App extends Component {
   constructor(){
@@ -12,10 +13,20 @@ class App extends Component {
   }
 
   render() {
+    const style = {
+      display: 'flex',
+      flexDirection: 'column',
+      minHeight: '100%',
+      width: '100%',
+      justifyContent: 'space-between'
+    }
     return (
-      <div>
-        <Header />
-        <MainBody />
+      <div style={style}>
+        <div>
+          <Header />
+          <MainBody />
+        </div>
+        <Footer />
       </div>
     )
   }
